edc9e1056868aac45394091010e130b3e11e90d1
Sachin <sachnaror@gmail.com>
2025-01-21 12:04:37 +0530

    Persistent effort yields boundless rewards

--- upload_video.py
+++ upload_video.py
@@ -47,7 +47,7 @@ def upload_to_youtube(video_path):
             credentials.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secrets.json', SCOPES
+                '/Users/homesachin/Desktop/zoneone/practice/shell_scripts/client_secrets.json', SCOPES
             )
             credentials = flow.run_local_server(port=0)
 
